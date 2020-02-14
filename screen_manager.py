fa6a25ce610399b721a908b35534b0f5936b8b9c
Kaylee <60186448+kaynam23@users.noreply.github.com>
2020-02-14 05:31:23 -0500

    fixed

--- screen_manager.py
+++ screen_manager.py
@@ -1,1 +1,30 @@
 # self.endGame() should give a summary screen. 
+from startScreen import Startscreen
+from game_Gui import Application
+from game_over import Application
+
+class screen_manager(object):
+    def __init__(self):
+        self.root = Tk()
+        self.current_screen = None
+        
+    
+    def start_screen(self):
+        self.root.title("Welcome!")
+        self.current_screen = Startscreen(self.root, self.start_button)
+    
+    def start_button(self):
+        self.current_screen.destroy()
+        self.game()
+
+    def game(self, yummywords.txt, self.endGame):
+        self.root.title("Typing Game!")
+        self.current_screen = Application(self.root, self.endGame)
+    
+    def endGame(self, yummywords.txt, self.exit_button):
+        self.current_screen.destroy()
+        self.root.title("Game Summary")
+        self.current_screen = summary(self.root)
+    
+    def exit_button(self):
+        self.current_screen.destroy()
